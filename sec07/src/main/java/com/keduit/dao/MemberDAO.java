1bdfd377dd5f8a188a0168f2aaa8c5d8bb6e6859
ByeongJunAn <abj5237@nate.com>
2022-12-21 13:28:32 +0900

    '2022-12-21'

--- sec07/src/main/java/com/keduit/dao/MemberDAO.java
+++ sec07/src/main/java/com/keduit/dao/MemberDAO.java
@@ -8,6 +8,8 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import com.keduit.dto.MemberVO;
+
 public class MemberDAO {
 	private MemberDAO() {
 		
@@ -58,6 +60,105 @@ public class MemberDAO {
 			}
 }
 		
+		return result;
+	}
+	
+// 아이디로 회원 정보를 가져오는 메소드
+	public MemberVO getMember(String userid) {
+		MemberVO mVO = null;
+		String sql = "select * from member where userid = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()) { 
+				mVO = new MemberVO();
+				mVO.setName(rs.getString("name"));
+				mVO.setUserid(rs.getString("userid"));
+				mVO.setPwd(rs.getString("pwd"));
+				mVO.setEmail(rs.getString("email"));
+				mVO.setPhone(rs.getString("phone"));
+				mVO.setAdmin(rs.getInt("admin"));
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		 
+		return mVO;
+	}
+
+	public int updateMember(MemberVO mVO) {
+		int result = -1;
+		String sql = "update member set pwd=?, email=?, phone=?, admin=? where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mVO.getPwd());
+			pstmt.setString(2, mVO.getEmail());
+			pstmt.setString(3, mVO.getPhone());
+			pstmt.setInt(4, mVO.getAdmin());
+			pstmt.setString(5, mVO.getUserid());
+			result = pstmt.executeUpdate();
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		
+		
+	}return result;
+		
+	}
+
+	public int confirmID(String userid) {
+		int result = -1;
+		String  sql = "select userid from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = 1;				
+			} else {
+				result = -1;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null ) pstmt.close();
+				if(conn != null) conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		return result;
 	}
 	
--- sec07/src/main/webapp/member/memberUpdate.jsp
+++ sec07/src/main/webapp/member/memberUpdate.jsp
@@ -1,33 +1,30 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>     
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>  
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>회원가입 </title>
+<title>Insert title here </title>
 <script type="text/javascript" src="script/member.js"></script>
+
 </head>
 <body>
-	
-	<h1> 회원 가입</h1>
 
-	<p> '*' 표시 항목은 필수 입력 항목 입니다.
-
-	<form action= "join.do" method="post" name="frm">
+		
+	<h1> 회원 수정</h1>
+	<form action= "memberUpdate.do" method="post" name="frm">
 		<table>
 			<tr>
 					<td> 이름 </td>
-					<td><input type="text" name="name" id="name" size="20">*</td>
+					<td><input type="text" name="name" id="name" size="20" value="${mVO.name}" readonly></td>
 			</tr>
 		
 			<tr>
 			
 				    <td>아 이 디</td>
-					<td><input type="text" name="userid" size="20">*
-					<input type="hidden" name="reid" size="20"> 
-					<input type="button" value="중복체크" onclick="idCheck()">
+					<td><input type="text" name="userid" size="20" value="${mVO.userid}" readonly>
 					</td>
 			</tr>
 					
@@ -48,21 +45,29 @@
 			
 			<tr>
 					<td>이메일</td>
-					<td><input type="text" name="email" id="email">
+					<td><input type="text" name="email" size="20" value="${mVO.email}">
 					</td>
 			</tr>
 			
 			<tr>
 					<td>전화번호</td>
-					<td><input type="text" name="phone" id="phone"></td>
-			
+					<td><input type="text" name="phone" size="20" value="${mVO.phone}">
+					</td>
+			</tr>
 			<tr>
 					<td>등급</td>
-					
+					<td><c:choose>
+						<c:when test="${mVO.admin==0}">
 					<td><input type="radio" name = "admin" value="0" checked> 일반회원 
 						<input type="radio" name = "admin" value="1"  > 관리자
+						</c:when>
+						<c:otherwise>
+							<input type="radio" name="admin" value="0"> 일반회원
+							<input type="radio" name="admin" value="1" checked="checked">관리자
+						</c:otherwise>
+					</c:choose>
 					</td>
-	
+					</tr>
 					<tr>
 						<td colspan="2" align="center">
 							<input type="submit" value="확인" onclick="return joinCheck()">
@@ -71,11 +76,10 @@
 						</td>
 					</tr>
 					
-					<tr>
-						<td colspan="2">${message}</td>
-					</tr>
+			
 		</table>
 	</form>
 	
+
 </body>
 </html>
